d68e065f8c343b13e2b126bb01f3c5c439763ea3
Paul Krusell <paul.krusell@students.olin.edu>
2017-03-27 20:00:48 -0400

    pushing keeper with rough blue filter

--- src/keeper.py
+++ src/keeper.py
@@ -12,6 +12,7 @@ import numpy as np
 from geometry_msgs.msg import Twist, Vector3
 if not cv2.__version__ == '3.1.0-dev':
     import cv2.cv as cv
+import math
 
 class Keeper(object):
     """ The Keeper is a Python object that encompasses a ROS node
@@ -41,11 +42,13 @@ class Keeper(object):
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         cv2.namedWindow('video_window')
         cv2.namedWindow('image_info')
-        cv2.setMouseCallback('video_window', self.process_mouse_event)
+        # cv2.setMouseCallback('video_window', self.process_mouse_event)
         self.center_x=None
         self.center_y = None
         self.circles = None
+        self.min_dist = 0
 
+        cv2.createTrackbar('Minimum Distance', 'video_window', 60, 255, self.set_min_dist)
         cv2.createTrackbar('param1', 'video_window', 0, 255, self.set_param1)
         cv2.createTrackbar('param2', 'video_window', 0, 255, self.set_param2)
         cv2.createTrackbar('min_radius_1', 'video_window', 0, 255, self.set_min_radius_1)
@@ -82,6 +85,10 @@ class Keeper(object):
         """ sets max radius"""
         self.max_radius_1 = val
 
+    def set_min_dist(self, val):
+        """ Sets min dist btw circles"""
+        self.min_dist = val
+
     def process_image(self, msg):
         """ Process image messages from ROS and stash them in an attribute
             called cv_image for subsequent processing """
@@ -93,24 +100,45 @@ class Keeper(object):
 
         #     self.center_x, self.center_y = moments['m10']/moments['m00'], moments['m01']/moments['m00']
         self.binary_image = cv2.medianBlur(self.binary_image, 5)
-        self.circles = cv2.HoughCircles(cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2GRAY),self.houghGrad,1,20,
+        self.circles = cv2.HoughCircles(cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2GRAY),self.houghGrad,1,self.min_dist,
                             param1=self.param1,param2=self.param2,minRadius=self.min_radius_1,maxRadius=self.max_radius_1)
         #self.circles = cv2.HoughCircles(self.binary_image,cv.CV_HOUGH_GRADIENT,1,20,
                             #param1=self.param1,param2=self.param2,minRadius=self.min_radius_1,maxRadius=self.max_radius_1)
         # cv2.circle(self.cv_image, (int(self.center_x),int(self.center_y)), 10, (255,255,255))
-        if not (self.circles == None):
-            for i in self.circles[0,:]:
-                # draw the outer circle
-                cv2.circle(self.cv_image,(i[0],i[1]),i[2],(0,255,0),2)
-                # draw the center of the circle
-                cv2.circle(self.cv_image,(i[0],i[1]),2,(0,0,255),3)
-                # print(len(self.circles))
+    
+    def blue_checker(self, circle):
+        """ checks each circle to see how blue it is at several pixels within the circle"""
+        thetas = (0, 45, 90, 135, 180, 125,270, 315)
+        s = 0
+        print("circle = ",circle)
+        for t in thetas:
+            y_cord = int(.75*circle[2]*math.sin(math.radians(t))+circle[0])
+            x_cord = int(.75*circle[2]*math.cos(math.radians(t))+circle[1])
+            if x_cord < 640 and y_cord < 480:
+                s += self.cv_image[y_cord, x_cord,0]
+        return s
+
 
     def run(self):
         """ The main run loop, in this node it doesn't do anything """
         r = rospy.Rate(5)
         while not rospy.is_shutdown():
 
+            if not (self.circles == None):
+                current_max = 0
+                for i,q in enumerate(self.circles[0,:]):
+                    # draw the outer circle
+                    print(q)
+                    if self.blue_checker(q) > current_max:
+                        current_max = self.blue_checker(q)
+                        ind = i
+
+
+
+                cv2.circle(self.cv_image,(self.circles[0, ind, 0],self.circles[0, ind, 1]),self.circles[0, ind, 2],(0,255,0),2)
+                    # draw the center of the circle
+                cv2.circle(self.cv_image,(self.circles[0, ind, 0],self.circles[0, ind, 1]),2,(0,0,255),3)
+                    # print(len(self.circles))
 
             if not self.binary_image is None:
                 cv2.imshow('video_window2', self.binary_image)
@@ -124,16 +152,16 @@ class Keeper(object):
             # start out not issuing any motor commands
             r.sleep()
 
-    def process_mouse_event(self, event, x,y,flags,param):
-        """ Process mouse events so that you can see the color values associated
-            with a particular pixel in the camera images """
-        self.image_info_window = 255*np.ones((500,500,3))
-        cv2.putText(self.image_info_window,
-                    'Color (b=%d,g=%d,r=%d)' % (self.cv_image[y,x,0], self.cv_image[y,x,1], self.cv_image[y,x,2]),
-                    (5,50),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    1,
-                    (0,0,0))
+    # def process_mouse_event(self, event, x,y,flags,param):
+    #     """ Process mouse events so that you can see the color values associated
+    #         with a particular pixel in the camera images """
+    #     self.image_info_window = 255*np.ones((500,500,3))
+    #     cv2.putText(self.image_info_window,
+    #                 'Color (b=%d,g=%d,r=%d)' % (self.cv_image[y,x,0], self.cv_image[y,x,1], self.cv_image[y,x,2]),
+    #                 (5,50),
+    #                 cv2.FONT_HERSHEY_SIMPLEX,
+    #                 1,
+    #                 (0,0,0))
 
 
 if __name__ == '__main__':
